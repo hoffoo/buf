2ae30f9bdfaefe0ee63c0b24cbb4a67a00f04d06
Marin <marin@stayke.net>
2014-03-30 12:51:53 -0700

    io cleanup

--- io.go
+++ io.go
@@ -1,30 +1,36 @@
 package buf
 
-// IO utils to create bufs
+// IO utils to create lines
 
 import (
     "io"
     "bufio"
 )
 
+// Read from in, first line is returned, the rest are joined under it.
+// returns error in case of scanner/io err.
 func ReadIO(in io.Reader) (l *Line, err error) {
+
     s := bufio.NewScanner(in)
 
-    for s.Scan() == true {
-
-        if l == nil {
-            l = &Line{data: s.Text()}
-            continue
-        }
-
-
-        if s.Err() != nil {
-            return nil, s.Err()
-        }
-
-        // TODO more efficient rather than append
-        l.Append(&Line{data: s.Text()})
+    // scan once to get the first line
+    if s.Scan() == false || s.Err() != nil {
+        return nil, s.Err()
     }
 
-    return
+    l = &Line{data: s.Text()}
+    scanIO(l, s)
+
+    return l, s.Err()
+}
+
+func scanIO(l *Line, s *bufio.Scanner) {
+
+    if s.Scan() == false || s.Err() != nil {
+        return
+    }
+
+    lnext := &Line{data: s.Text()}
+    l.Join(lnext)
+    scanIO(lnext, s)
 }
